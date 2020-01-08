4307cd33e3a9406fce3652b48286619c34aebafb
Jin Wu <jinn.wuu@gmail.com>
2020-01-08 03:55:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   def add_student(student_name, grade)
     if @roster[grade] == nil
       @roster[grade] = []
-      @roster[grade] << student_name
+        @roster[grade] << student_name
     else
       @roster[grade] << student_name
     end
