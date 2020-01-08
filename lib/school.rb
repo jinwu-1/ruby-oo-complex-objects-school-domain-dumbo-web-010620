82fc0648d248819f60a642ff268033102fb29a1a
Jin Wu <jinn.wuu@gmail.com>
2020-01-08 03:58:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,8 @@ class School
   end
   
   def add_student(student_name, grade)      # roster => {9 => ["Zach Morris"]}
-    if @roster[grade] == nil                # if it is nil then
-      @roster[grade] = []                   # returns empty array
-        @roster[grade] << student_name      # add student_name to empty array
-    else
-      @roster[grade] << student_name
-    end
+    @roster[grade] = {}
+    @roster[grade] << student name
   end
   
 end
